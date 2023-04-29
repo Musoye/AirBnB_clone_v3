d967b9bf96ebfd5140365663e13fddbb59f0f794
Musoye <oyebamijimustapha44@gmail.com>
2023-04-29 09:08:58 -0700

    feat: make the status api return ok

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,20 +4,21 @@
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
-from os import getenv
+import os
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_app():
+def close_app(exc):
     """when the app is close"""
     storage.close()
 
 
 if __name__ == "__main__":
-    host = getenv(HBNB_API_HOST) if getenv(HBNB_API_HOST) else "0.0.0.0"
-    port = getenv(HBNB_API_PORT) if getenv(HBNB_API_PORT) else 50000
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = os.environ.get('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
